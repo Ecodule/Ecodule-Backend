668fc30cb643770dda548465a1ee121032ba85dc
Meri11Ra1 <r.t.00293794@gmail.com>
2025-09-06 15:15:37 +0900

    refresh_tokenテーブルのcrud、createとdeleteを追加

--- src/crud/refresh_token.py
+++ src/crud/refresh_token.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
+from datetime import datetime, timedelta, timezone
 
-import models.user, core.auth as auth # 先ほど作成したauth.py
+from core.token import REFRESH_TOKEN_EXPIRE_DAYS, create_refresh_token
+import models.user
 import schemas.user
 
 def get_user_by_refresh_token(db: Session, refresh_token: str):
@@ -12,4 +14,40 @@ def get_user_by_refresh_token(db: Session, refresh_token: str):
 
 def create_refresh_token(db: Session, user_id: str) -> schemas.user.UserTokenResponse:
     # create a new refresh token and store it in the database
-    new_token = auth.create_refresh_token()
+    user = db.query(models.user.User).filter(models.user.User.id == user_id).first()
+
+    if not user:
+        raise ValueError("User not found")
+    
+    if not user.is_active:
+        raise ValueError("User is not active")
+
+    new_refresh_token = models.user.RefreshToken(
+        user_id=user.id,
+        token=create_refresh_token(),
+        created_at=datetime.now(timezone.utc),
+        expires_at=datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS),
+    )
+
+    db.add(new_refresh_token)
+    db.commit()
+    db.refresh(new_refresh_token)
+
+    return new_refresh_token
+
+def revoke_refresh_token(db: Session, user_id: str):
+    # Revoke the existing refresh token for the user
+    user = db.query(models.user.User).filter(models.user.User.id == user_id).first()
+
+    if not user:
+        raise ValueError("User not found")
+    
+    if not user.refresh_token:
+        return  # No refresh token to revoke
+    
+    user.refresh_token.is_revoked = True
+    db.add(user.refresh_token)
+    db.commit()
+    db.refresh(user.refresh_token)
+
+    return user.refresh_token
