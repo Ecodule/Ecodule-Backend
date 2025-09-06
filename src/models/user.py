77931677d8e7d82c934944cb199c11aa1108ca8b
Meri11Ra1 <r.t.00293794@gmail.com>
2025-09-07 01:28:43 +0900

    refresh_tokensテーブルのカラムtokenをhashed_tokenに変更

--- src/models/user.py
+++ src/models/user.py
@@ -47,7 +47,7 @@ class RefreshToken(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
-    token = Column(String, unique=True, nullable=False)
+    hashed_token = Column(String, unique=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
     expires_at = Column(DateTime, nullable=False)
     is_revoked = Column(Boolean, default=False)
