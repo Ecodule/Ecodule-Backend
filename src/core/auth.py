3c284111c36a6ac8fdf5af17bf8e7a49e1ce3233
Meri11Ra1 <r.t.00293794@gmail.com>
2025-09-06 14:45:49 +0900

    coreのauthメソッド群をファイル分割

--- src/core/auth.py
+++ src/core/auth.py
@@ -1,50 +1,21 @@
 import os
-from datetime import datetime, timedelta, timezone
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from jose import JWTError, jwt
-from passlib.context import CryptContext
 from dotenv import load_dotenv
-import secrets
 
 import crud.user
 from db.session import get_db
 
 load_dotenv()
 
-# specify the hashing algorithm as bcrypt
-# the deprecated option is set to automatically use the latest algorithm
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    # Compare the plain password with the hashed password
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password: str) -> str:
-    # Generate a hash from the plain password
-    return pwd_context.hash(password)
-
 # JWT settings
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
-REFRESH_TOKEN_EXPIRE_DAYS = 60
 
 # define the rule to get token from the request
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
-
-def create_access_token(data: dict):
-    # Generate a JWT token
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-def create_refresh_token():
-    # Generate a secure random string as refresh token
-    return secrets.token_urlsafe(32)
 
 # Decode the JWT token to get the current user
 async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
