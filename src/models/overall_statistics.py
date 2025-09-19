3f304125abd4343a95fddf38e365c2a84a4f9c14
Meri11Ra1 <r.t.00293794@gmail.com>
2025-09-20 03:06:02 +0900

    全体統計テーブルの追加とマイグレーション

---
+++ src/models/overall_statistics.py
@@ -0,0 +1,10 @@
+from sqlalchemy import UUID, Column, Float, DateTime
+from sqlalchemy.sql import func
+from db.session import Base
+
+class OverallStats(Base):
+    __tablename__ = 'overall_stats'
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    total_money_saved = Column(Float)
+    total_co2_reduction = Column(Float)
+    calculated_at = Column(DateTime, default=func.now())
